080ae0d73e61558d7160afee1856ccd282aa8dbe
Литвинович Владислав <vladkefir2003@mail.ru>
2024-11-20 15:46:01 +0300

    Firebase libraries add

--- app/src/main/java/com/example/soread/screens/RegistrationScreen.kt
+++ app/src/main/java/com/example/soread/screens/RegistrationScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.soread.components.registration.*
 
@@ -27,4 +28,10 @@ fun RegistrationScreen() {
         RegistrationButton()
 
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegPreview(){
+    RegistrationScreen()
 }
